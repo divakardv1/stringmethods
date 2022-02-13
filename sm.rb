f339be31fd63140aab48f038f4d88293577ab203
divakardv1 <98635047+divakardv1@users.noreply.github.com>
2022-02-13 12:05:21 +0530

    Add files via upload

---
+++ sm.rb
@@ -0,0 +1,55 @@
+#string concatination
+a = "Hello"
+b = " World!"
+puts a + b
+
+#upcase and downcase methods
+puts a.upcase
+puts b.downcase
+
+
+#capitalize method
+c = "divakar"
+puts c.capitalize
+
+
+#swapcase mthods
+d = "DivAKaR"
+puts d.swapcase
+
+#length and reverse 
+puts d.length
+puts a.reverse
+
+#indexing 
+puts a[2]
+
+
+
+#finding sub string 
+
+e = "welcome to ruby"
+f = "come"
+
+puts e.include? f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
